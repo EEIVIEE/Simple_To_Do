5deb3b1399b4f730480a31b1cd8e2274ae8a4d9d
Louis Puah <louispyh@gmail.com>
2022-06-03 14:18:40 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21021397/simpletodo/MainActivity.java
@@ -0,0 +1,104 @@
+package sg.edu.rp.c346.id21021397.simpletodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spnTask;
+    EditText etTask;
+    Button btnAdd;
+    Button btnDelete;
+    Button btnClear;
+    ListView lvTasks;
+    ArrayList<String> alTasks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spnTask = findViewById(R.id.spinnerTask);
+        etTask = findViewById(R.id.editTextTasks);
+        btnAdd = findViewById(R.id.buttonAdd);
+        btnDelete = findViewById(R.id.buttonDelete);
+        btnClear = findViewById(R.id.buttonClear);
+        lvTasks = findViewById(R.id.listViewTasks);
+        alTasks = new ArrayList<>();
+
+        ArrayAdapter aaTasks = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alTasks);
+
+        lvTasks.setAdapter(aaTasks);
+
+        spnTask.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        btnAdd.setEnabled(true);
+                        btnDelete.setEnabled(false);
+                        etTask.setHint(R.string.enter_task);
+                        break;
+                    case 1:
+                        btnAdd.setEnabled(false);
+                        btnDelete.setEnabled(true);
+                        etTask.setHint(R.string.remove_task);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String element = etTask.getText().toString();
+                alTasks.add(element);
+                aaTasks.notifyDataSetChanged();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(alTasks.size() > 0){
+                    int pos = Integer.parseInt(etTask.getText().toString());
+                    if(pos + 1 > alTasks.size()){
+                        Toast.makeText(getApplicationContext(),R.string.wrong_index,Toast.LENGTH_SHORT).show();
+                    }else{
+                        alTasks.remove(pos);
+                        aaTasks.notifyDataSetChanged();
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(),R.string.list_empty,Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alTasks.clear();
+                aaTasks.notifyDataSetChanged();
+            }
+        });
+
+    }
+}
